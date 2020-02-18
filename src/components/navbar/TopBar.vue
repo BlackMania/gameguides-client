00391691549fc3965011fac231efbcf9285cc530
BlackMania <BlackMania@users.noreply.github.com>
2020-02-18 11:55:04 +0100

    First Design TopBar

---
+++ src/components/navbar/TopBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-card tile>
+        <v-toolbar height="25px" color="primary">
+            <v-menu open-on-hover bottom offset-y>
+                <template v-slot:activator="{ on }">
+                    <v-btn class="pl-2 pr-2 grey--text"
+                           tile
+                           v-on="on"
+                           height="inherit"
+                           width="auto"
+                           icon
+                    >
+                        <v-icon>mdi-gamepad-variant-outline</v-icon>
+                        <v-icon>mdi-menu-down</v-icon>
+                    </v-btn>
+                </template>
+                <v-card tile>
+                    <v-list tile>
+                        <v-list-item>League of Legends</v-list-item>
+                        <v-list-item>League of Legends</v-list-item>
+                    </v-list>
+                </v-card>
+            </v-menu>
+            <div>
+                test
+            </div>
+        </v-toolbar>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: "TopBar",
+        created() {
+            window.console.log(this.$parent);
+        }
+    }
+</script>
+
+<style scoped>
+    .v-menu__content {
+        border-radius: 0 !important;
+        left: 0 !important;
+    }
+</style>
