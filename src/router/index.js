7b99e3088ffc19dd7b0b0d8cc480018128060f90
BlackMania <BlackMania@users.noreply.github.com>
2020-02-24 11:51:46 +0100

    Added main route

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import LandingIndex from "@/components/landing/LandingIndex";
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'landing',
+    component: LandingIndex
+  }
 ]
 
 const router = new VueRouter({
