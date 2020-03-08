44d861ee967e840d1f5289634cc1e8a59f4d00ec
Vincent <vincentmuijtjens@ziggo.nl>
2020-03-08 21:56:13 +0100

    Added some store values for league

--- src/store/lolstore.js
+++ src/store/lolstore.js
@@ -1,16 +1,45 @@
 // League of Legends Store
 const lolStore = {
     state: {
-
+        championData: [],
+        freeChampionRotation: []
     },
     mutations: {
-
+        SET_CHAMPION_DATA(state, data) {
+            state.championData = data;
+        },
+        SET_FREE_CHAMPION_ROTATION(state, data) {
+            if(state.championData !== [] || state.championData !== undefined)
+            {
+                let newData = [];
+                for(const champion in state.championData)
+                {
+                    let d = state.championData[champion];
+                    if(d.key !== undefined)
+                    {
+                        if(data.freeChampionIds.includes(parseInt(d.key)))
+                        {
+                            newData.push(state.championData[champion]);
+                        }
+                    }
+                }
+                state.freeChampionRotation = newData;
+            }
+        }
     },
     actions: {
-
+        setChampionData ({ commit }, games) {
+            commit('SET_CHAMPION_DATA', games);
+        },
+        setFreeChampionRotation({commit}, champions) {
+            commit('SET_FREE_CHAMPION_ROTATION', champions);
+        }
     },
     modules: {
 
+    },
+    getters: {
+        freeChampionRotation: state => state.freeChampionRotation
     }
 }
 
