f156e17db108e85e9f44d27302f27004ebb06c6e
BlackMania <BlackMania@users.noreply.github.com>
2020-05-04 15:25:57 +0200

    Added extra checks before route

--- src/router/index.js
+++ src/router/index.js
@@ -39,30 +39,42 @@ const routes = [
                 component: CreateGuideSelector,
                 props: (route) => ({game: route.params.game}),
                 beforeEnter(to, from, next) {
-                    let newGuideObject = {
-                        uuid: "",
-                        patchversion: "",
-                        skills: new Array(18),
-                        runeset: {
-                            mainset: new Array(5),
-                            secondset: new Array(3),
-                            statmods: new Array(3),
-                        },
-                        title: "",
-                        content: "",
-                        champion: "Aatrox",
-                        lane: "",
-                        madeby: {
-                            uuid: "",
-                            username: "",
-                        },
-                        views: 0,
-                        upvotes: 0,
-                        created: "",
-                        updates: "",
-                    };
-                    store.dispatch('setSelectedGuide', newGuideObject);
-                    next();
+                    if(router.app.$session.exists())
+                    {
+                        if(router.app.$session.get('token') != null)
+                        {
+                            let token = router.app.$session.get('token');
+                            let payload = token.split('.')[1];
+                            let unencoded = atob(payload);
+                            let parsedToken =  JSON.parse(unencoded);
+                            let newGuideObject = {
+                                uuid: "",
+                                patchversion: "",
+                                skills: new Array(18),
+                                runeset: {
+                                    mainset: new Array(5),
+                                    secondset: new Array(3),
+                                    statmods: new Array(3),
+                                },
+                                title: "",
+                                content: "",
+                                champion: "Aatrox",
+                                lane: "",
+                                madeby: {
+                                    uuid: parsedToken.clientid,
+                                    username: parsedToken.sub,
+                                },
+                                views: 0,
+                                upvotes: 0,
+                                created: "",
+                                updates: "",
+                            };
+                            store.dispatch('setSelectedGuide', newGuideObject);
+                            next();
+                        }
+                    } else {
+                        alert('You must be logged in to perform this action');
+                    }
                 }
             }
         ]
