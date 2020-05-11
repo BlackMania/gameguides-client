5aaba7d02325cd4b17fd2a31d96b6ec0538d8a2d
Vincent <vincentmuijtjens@ziggo.nl>
2020-05-11 12:58:54 +0200

    Added vuetify mock for unit tests

---
+++ _mock_/vuetify.mock.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
+
+const vuetify = new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#0d111a',
+                secondary: '#2a2e37',
+                tertiary: '#0f131c',
+                accent: '#8c9eff',
+                error: '#b51209',
+                background: '#1b262c',
+                text: '#535966'
+            },
+        },
+    }
+})
+
+export default vuetify
